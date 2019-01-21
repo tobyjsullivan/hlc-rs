798dbc6fc0af5f0f42d429773414e00e05c63ecf
Toby Sullivan <tobyjsullivan@gmail.com>
2019-01-21 22:25:24 +1100

    .

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM alpine:3.6
+FROM amd64/alpine:3.6
 
 ADD bin/start.sh start.sh
 RUN chmod u+x start.sh
--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,9 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 main() {
   local data_dir="$1"
   local data_file="$2"
   local prog="$3"
-  uname -s
-  uname -m
-  ldd --version
 
   mkdir -p "$data_dir"
   unzip "$data_file" -d "$data_dir"
